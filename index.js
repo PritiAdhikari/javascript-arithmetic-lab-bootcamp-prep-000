bd21e0db69fd9370e8abefc3c8a4df28accfca35
pritiadhikari@gmail.com <pritiadhikari@gmail.com>
2018-06-25 23:14:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,24 +30,12 @@ function dec(a)
 
 function makeInt(n)
 {
-<<<<<<< HEAD
-  
-=======
-   n='2';
->>>>>>> d8b2f985b001c245f0d5eb48ccb31053dde8b26d
+
   return parseInt(n,10)
 }
 
-
-<<<<<<< HEAD
 function preserveDecimal(n)
 {
   
   return parseFloat(n,10)
 }
-
-
-
-
-=======
->>>>>>> d8b2f985b001c245f0d5eb48ccb31053dde8b26d
